1d0951326e370c38d14d7fe8d118e10d174b769b
Vasiliy Poltorak <zloyrusvelt@gmail.com>
2022-04-05 15:21:55 +0300

    Setup project

--- Authorization/LoginViewController.swift
+++ Authorization/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Authorization
 //
 //  Created by Василий Полторак on 05.04.2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
